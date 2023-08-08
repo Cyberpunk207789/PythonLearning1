d68472b5e627d548b973d9cc510d448fc14a8750
Cyberpunk207789 <robloxfan0008885@gmail.com>
2023-08-08 15:47:14 +0300

    Функция использует 2 итератора(с начала и с конца) и сравнивает "

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def Palindrom (string: str) -> bool:
+    right = len(string) - 1
+    left = 0
+    while right != left and right - left != 1:
+        s1 = string[right]
+        s2 = string[left]
+        if not s1 == s2:
+            return False
+        right -= 1
+        left += 1
+    return True
+
+print(Palindrom("лессооссел"))
